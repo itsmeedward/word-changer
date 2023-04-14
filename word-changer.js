109958563ee1662b3281e260e16b2a91b22e187c
itsmeedward <86776978+itsmeedward@users.noreply.github.com>
2023-04-14 09:08:17 +0300

    Add files via upload
    Have some fun !

---
+++ word-changer.js
@@ -0,0 +1,40 @@
+const express= require("express")
+const app = express()
+
+const port = 3999
+
+app.get('/',(req,res)=>{
+    res.send("hey there!")
+})
+
+try {
+    app.listen(port,()=>{
+        console.log(`the server is runing at port${port}`)
+    })
+} catch (error) {
+    throw(error)
+}
+    
+    var output= "i love you"
+    
+
+    output.slice()
+
+    console.log(output)
+
+    let output2= output.split(" ").join("")
+
+    output2.toLowerCase
+    console.log(output2.toLowerCase)
+    
+    function crypto(output) {
+        
+        for (let i = 0; i < output.length; i++) {
+            const element = output[i];
+              
+    
+        }
+    }
+  
+    console.log(output.toUpperCase)
+   
